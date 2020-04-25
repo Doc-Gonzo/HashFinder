a533f26f595560ecb791c1882f9687b4b3e81633
Mathias Schaller <stoertebecker@gmx.at>
2020-04-25 17:17:24 +0200

    Multithreading: 2 Richtungen

--- src/com/company/HashFinder.java
+++ src/com/company/HashFinder.java
@@ -8,32 +8,55 @@ public class HashFinder implements Runnable{
     // Shared Result Object
     Result result;
     String threadName,data,difficulty;
-    long nonce;
+    long nonce,nonceOffset,searchRange;
+    boolean ascending;
 
     // Konstruktor
-    HashFinder(Result r, String threadName,String data, String difficulty, long nonce) {
+    HashFinder(Result r, String threadName,String data, String difficulty, long nonce,long nonceOffset,boolean ascending) {
         this.result = r;
         this.threadName = threadName;
         this.data = data;
         this.difficulty = difficulty;
         this.nonce = nonce;
+        // Anzahl der generierten Hashes
+        this.nonceOffset = nonceOffset;
+        // Suchrichtung
+        this.ascending=ascending;
+        this.searchRange = nonceOffset / 2;
 
         new Thread(this, this.threadName).start();
     }
     // Run Methode checkt ob Hash im shared objekt gefunden
     public void run() {
+        // Wenn Suchrichtung absteigend, setze nonce auf max
+        if (!ascending) {
+            nonce = nonceOffset;
+        }
+        // Suche Bereich ab
         while (!result.isFound()) {
             do {
+                // Checke ob Suchrange erschöpft, starte dan nerneut mit 10fachem Range + offset, um doppelte verarbeitung der ersten Ranges zu vermeiden
+                if (nonce == searchRange) {
+                    nonce = searchRange *10 + nonce;
+                }
                 String strToHash = data+nonce;
                 String hash = calculateHash(strToHash);
-                System.out.printf("%s - %s %n",Thread.currentThread().getName(),hash,"Nonce:" , nonce);
+               // System.out.println("Nonce" + nonce);
+                System.out.printf("%s - %s %n",Thread.currentThread().getName(),hash + "    Nonce Nr: " + nonce);
                 if(hash.startsWith(difficulty)){
                     result.setFound(true);
                     result.setSuccessfullThread(Thread.currentThread().getName());
                     result.setHash(hash);
-                    System.out.printf("Hash gefunden:  %s - %s %n",Thread.currentThread().getName(),hash,"Nonce:" + nonce);
+                    System.out.printf("Hash gefunden:  %s - %s %n",Thread.currentThread().getName(),hash);
                 }
-                nonce++;
+                // Checke ob absteigend
+                if (ascending){
+                    nonce++;
+                }
+                else  {
+                    nonce--;
+                }
+
             } while (!result.isFound() && nonce < Long.MAX_VALUE);
         }
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,22 @@ public class Main {
     public static void main(String[] args) {
         // Ergebnisobjekt, auf das alle Threads zugreifen könnnen
         Result result = new Result();
+        String data = "Manufaktur";
+        String difficulty = "0000";
+        // Beginne immer bei "0"
+        long nonce = 0;
+        // Anzahl an generierten Hashes
+        long nonceOffset = 10000;
+
 
         System.out.println("Hello World!");
         // HashFinder.findHash("Hello","0000");
 
-        HashFinder finder1 = new HashFinder(result,"thread1","Hello","0000",0);
-        HashFinder finder2 = new HashFinder(result,"thread2","Hello","0000",5000);
+
+        HashFinder finder1 = new HashFinder(result,"thread1",data,difficulty,nonce,nonceOffset,true);
+        HashFinder finder2 = new HashFinder(result,"thread2",data,difficulty,nonceOffset,nonceOffset,false);
+
+
     }
 
 }
